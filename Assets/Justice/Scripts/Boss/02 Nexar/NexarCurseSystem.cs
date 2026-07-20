f69180b0011e801ea23907553a4ac8d9801eefca
lobin2017 <lobin2017@gmail.com>
2026-07-20 16:23:19 +0900

    Update

--- Assets/Justice/Scripts/Boss/02 Nexar/NexarCurseSystem.cs
+++ Assets/Justice/Scripts/Boss/02 Nexar/NexarCurseSystem.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Player;
 
-public class NexarCurseSystem : MonoBehaviour
+namespace Nexar
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum CurseType
     {
-        
+        None,
+        Faith,
+        Endurance,
+        Devotion,
+        Duty
     }
 
-    // Update is called once per frame
-    void Update()
+    public class NexarCurseSystem : MonoBehaviour
     {
+        private List<CurseType> activeCurses;
         
+        private void Awake()
+        {
+            activeCurses = new List<CurseType>();
+        }
+
+        public void ApplyCurse(CurseType curse)
+        {
+            activeCurses.Add(curse);
+        }
+
+        public void RemoveCurse(CurseType curse)
+        {
+            activeCurses.Remove(curse);
+        }
+
+        public bool HasCurse(CurseType curse)
+        {
+            return activeCurses.Contains(curse);
+        }
+
     }
 }
